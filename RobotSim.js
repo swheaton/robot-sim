686dba924caf10bc47041f091178b354059382f7
Stuart <swheaton5@gmail.com>
2017-02-18 19:33:19 +0000

    animation added plus position kinematics

--- RobotSim.js
+++ RobotSim.js
@@ -5,6 +5,15 @@ var gridHeight = pixelsPerFt * 15;
 
 var context = document.getElementById("myCanvas").getContext("2d");
 
+window.requestAnimFrame = (function(callback) {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame || window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+    function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
 var robotSpecs = 
 {
     "width": pixelsPerFt * 2,
@@ -31,13 +40,12 @@ var control =
     wheel4: 0
 }
 
-function drawBoard()
+function drawGrid()
 {
     for (var x = 0; x <= gridWidth; x += pixelsPerFt) {
         context.moveTo(0.5 + x, 0);
         context.lineTo(0.5 + x, gridHeight);
     }
-
 
     for (var x = 0; x <= gridHeight; x += pixelsPerFt) {
         context.moveTo(0, 0.5 + x);
@@ -48,6 +56,7 @@ function drawBoard()
     context.stroke();
 }
 
+// Draws robot given its 
 function drawRobot()
 {
 	var ctx=document.getElementById("myCanvas").getContext("2d");
@@ -61,17 +70,59 @@ function drawRobot()
 	ctx.restore();
 }
 
-// Update robot state given control signals and last state
-function updateState()
+function updateRobotPosition(timeDiff)
 {
-    robotState.velX = robotState.velX + (robotSpecs.wheelRadius/4) *
-        (control.wheel1-control.wheel2-control.wheel3+control.wheel4);
-    robotState.velY = robotState.velY + (robotSpecs.wheelRadius/4) *
-        (control.wheel1+control.wheel2+control.wheel3+control.wheel4);
-    robotState.velRot = robotState.velRot + robotSpecs.wheelRadius/
-        (4*(robotSpecs.width/2 + robotSpecs.height/2)) *
-        (-control.wheel1+control.wheel2-control.wheel3+control.wheel4)
+    robotState.row = robotState.row - robotState.velY * (10 / 1000.0);
+    robotState.col = robotState.col + robotState.velX * (10 / 1000.0);
+    robotState.heading = (robotState.heading + 
+        robotState.velRot * (100 / 1000.0)) % (Math.PI * 2);
 }
 
-drawBoard();
+// Update robot state given control signals and last state
+function updateRobotState(timeDiff)
+{
+    robotState.velX = (robotSpecs.wheelRadius/4) *
+        (control.wheel1-control.wheel2-control.wheel3+control.wheel4);
+    robotState.velY = (robotSpecs.wheelRadius/4) *
+        (control.wheel1+control.wheel2+control.wheel3+control.wheel4);
+    robotState.velRot = robotSpecs.wheelRadius/
+        (4*(robotSpecs.width/2 + robotSpecs.height/2)) *
+        (-control.wheel1+control.wheel2-control.wheel3+control.wheel4);
+    
+    updateRobotPosition(timeDiff);
+}
+
+function updateCanvas(canvas, timeDiff)
+{
+    // Clear the canvas before drawing
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawGrid();
+    updateRobotState(timeDiff);
+    drawRobot();
+}
+
+// Animate the frame
+function animate(canvas, context, currTime)
+{
+    // update
+    var time = (new Date()).getTime();
+    var timeDiff = time-currTime;
+
+    updateCanvas(document.getElementById("myCanvas"), timeDiff);
+
+    // request new frame
+    requestAnimationFrame(function() {
+        animate(canvas, context, time);
+    });
+}
+
+// Initial drawing
+drawGrid();
 drawRobot();
+
+// wait one second before starting animation
+setTimeout(function() {
+    var startTime = (new Date()).getTime();
+    animate(document.getElementById("myCanvas"), context, startTime);
+}, 1000);

4bf5af126b59659f761461631d68f2ed7773acd4
Stuart <swheaton5@gmail.com>
2017-02-18 18:22:06 +0000

    added kinematic equations for updating state. not used yet

--- RobotSim.js
+++ RobotSim.js
@@ -8,14 +8,27 @@ var context = document.getElementById("myCanvas").getContext("2d");
 var robotSpecs = 
 {
     "width": pixelsPerFt * 2,
-    "height": pixelsPerFt * 4
+    "height": pixelsPerFt * 4,
+    wheelRadius: 1
 }
 
 var robotState = 
 {
     "row": gridHeight/2,
     "col": gridWidth/2,
-    "heading": 0
+    "heading": 0,
+    velX: 0,
+    velY: 0,
+    velRot: 0
+}
+
+// Angular rotation rate of wheel, positive means going forward
+var control = 
+{
+    wheel1: 0,
+    wheel2: 0,
+    wheel3: 0,
+    wheel4: 0
 }
 
 function drawBoard()
@@ -46,6 +59,18 @@ function drawRobot()
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, -robotSpecs.height/2+5, robotSpecs.width/10, robotSpecs.height/10);
 	ctx.restore();
+}
+
+// Update robot state given control signals and last state
+function updateState()
+{
+    robotState.velX = robotState.velX + (robotSpecs.wheelRadius/4) *
+        (control.wheel1-control.wheel2-control.wheel3+control.wheel4);
+    robotState.velY = robotState.velY + (robotSpecs.wheelRadius/4) *
+        (control.wheel1+control.wheel2+control.wheel3+control.wheel4);
+    robotState.velRot = robotState.velRot + robotSpecs.wheelRadius/
+        (4*(robotSpecs.width/2 + robotSpecs.height/2)) *
+        (-control.wheel1+control.wheel2-control.wheel3+control.wheel4)
 }
 
 drawBoard();
